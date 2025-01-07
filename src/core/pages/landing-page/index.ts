303c65ceed53da68950902f1fb29651f8f962871
{zufichris} <{zufichris@gmail.com}>
2025-01-07 19:45:18 +0400

    Updated Landing Page

--- src/core/pages/landing-page/index.ts
+++ src/core/pages/landing-page/index.ts
@@ -1,3 +1,4 @@
 export { default as HeroSection } from "./hero-section"
+export { default as FeaturedCategories } from "./featured-categories"
 export { default as MostPopularProducts } from "./most-popular"
 export { default as LoadMore } from './load-more'
